03efc7a6457cf2f3c96dc2b3e36e44137aad8102
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2017-09-15 18:22:40 +0530

    Fix transition reflow bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <app-header />
-    <transition name="slide-fade">
+    <transition name="slide-fade" mode="out-in">
       <router-view></router-view>
     </transition>
   </div>
