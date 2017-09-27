fe7640c84d57457151adbbaf7f145d002e19735c
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2017-09-27 20:06:45 +0530

    Fix icon paths

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,7 +1,7 @@
-export { default as foodStall } from './food-stall.png';
+export { default as foodStall } from './food-stall.svg';
 export { default as groceries } from './groceries.svg';
-export { default as restaurant } from './restaurant.png';
-export { default as salad } from './salad.png';
-export { default as stew } from './stew.png';
-export { default as store } from './store.png';
-export { default as vegetables } from './vegetables.png';
+export { default as restaurant } from './restaurant.svg';
+export { default as salad } from './salad.svg';
+export { default as stew } from './stew.svg';
+export { default as store } from './store.svg';
+export { default as vegetables } from './vegetables.svg';
